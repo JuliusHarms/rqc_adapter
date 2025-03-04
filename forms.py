d0323f8d6407ea440ab02faa12f70bd3c3a0c692
Julius Harms <juliusharms@Juliuss-MBP.fritz.box>
2025-03-04 02:27:56 +0100

    Added django form instead of html form

--- forms.py
+++ forms.py
@@ -1,5 +1,30 @@
 from django import forms
+from django.core.validators import RegexValidator
+
+from plugins.rqc_adapter.utils import call_mhs_apikeycheck
 
 
 class DummyManagerForm(forms.Form):
     dummy_field = forms.CharField()
+
+class RqcSettingsForm(forms.Form):
+    journal_id_field = forms.IntegerField(error_messages={'invalid': 'Journal ID must be a number'})
+    journal_api_key_field = forms.CharField(max_length=64, min_length=1, validators=[RegexValidator(regex='^[0-9A-Za-z]$',message='The API key must only contain alphanumeric characters.')])
+
+    #Validate submitted journal_id and api_key together
+    #should there be an option to only submit one or the other?
+    #add default values if values are already present
+    def clean(self):
+        cleaned_data = super().clean()
+        journal_id = cleaned_data.get('journal_id_field')
+        api_key = cleaned_data.get('journal_api_key_field')
+        if journal_id and api_key:
+            try:
+                call_result = call_mhs_apikeycheck(journal_id, api_key)
+                if not call_result["success"]:
+                    error_msg = call_result['message']
+                    raise forms.ValidationError(error_msg)
+            #Generic Exception
+            except Exception as e:
+                raise forms.ValidationError("Unable to verify API key")
+        return cleaned_data
--- views.py
+++ views.py
@@ -1,12 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
+from core.models import SettingValue
 from plugins.rqc_adapter import forms
 from plugins.rqc_adapter.utils import set_journal_id, set_journal_api_key
 
 
 def manager(request):
-    form = forms.DummyManagerForm()
     template = 'rqc_adapter/manager.html'
     context = {
         'form': form,
@@ -14,12 +14,16 @@ def manager(request):
     return render(request, template, context)
 
 def handle_journal_id_settings_update(request):
-    if request.method != 'POST':
-        return JsonResponse({'error': 'Method not allowed'}, status=405)
+    if request.method == 'POST':
+        form = forms.RqcSettingsForm(request.POST)
+        if form.is_valid():
+            journal_id = request.data.get('journal_id')
+            set_journal_id(journal_id)
+            journal_api_key = request.data.get('api_key')
+            set_journal_api_key(journal_api_key)
+        return redirect('rqc_adapter_manager')
     else:
-        # validate keys beforehand
-        journal_id = request.data.get('journal_id')
-        set_journal_id(journal_id)
-        journal_api_key = request.data.get('api_key')
-        set_journal_api_key(journal_api_key)
-    return redirect('rqc_adapter_manager')
+        journal_id = SettingValue.objects.get(setting__name='rqc_journal_id')
+        journal_api_key = SettingValue.objects.get(setting__name='rqc_journal_api_key')
+        form = forms.RqcSettingsForm(initial={'journal_id_field': journal_id, 'journal_api_key_field': journal_api_key})
+        return render(request,"rqc_adapter/manager.html",{'form':form})
