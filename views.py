1d05e1adb781f47dc8a75857a316882d2b577ee2
Julius Harms <juliusharms@87-77-82-48.mna.fu-berlin.de>
2025-03-04 18:00:25 +0100

    Init form with default values

--- views.py
+++ views.py
@@ -8,12 +8,16 @@ from plugins.rqc_adapter.utils import set_journal_id, set_journal_api_key
 
 def manager(request):
     template = 'rqc_adapter/manager.html'
-    context = {
-        'form': form,
-    }
-    return render(request, template, context)
+    journal_id = SettingValue.objects.get(setting__name='rqc_journal_id')
+    journal_api_key = SettingValue.objects.get(setting__name='rqc_journal_api_key')
+    if journal_id.value and journal_api_key.value:
+        form = forms.RqcSettingsForm(initial={'journal_id_field': journal_id.value, 'journal_api_key_field': journal_api_key.value})
+    else:
+        form = forms.RqcSettingsForm()
+    return render(request, template, {'form': form})
 
 def handle_journal_id_settings_update(request):
+    print("test")
     if request.method == 'POST':
         form = forms.RqcSettingsForm(request.POST)
         if form.is_valid():
