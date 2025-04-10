fa03af849f61e02cd90b646e053f06acb57dd479
Julius Harms <julius-harms@web.de>
2025-04-10 19:44:56 +0200

    changed field name

--- models.py
+++ models.py
@@ -1,25 +1,21 @@
 from datetime import timezone, datetime
 
 from django.db import models
-from django.contrib.auth.models import User
 
 from core.janeway_global_settings import AUTH_USER_MODEL
 
 
 class RQCReviewerOptingDecision(models.Model):
-    # TODO add preliminary opting statuses - what do they do? -> used if a draft of the form is saved?
-    # opting status needs to be year specific
     class OptingChoices(models.IntegerChoices):
         UNDEFINED = 30, "RQC_OPTING_STATUS_UNDEFINED"
         OPT_IN = 31, "RQC_OPTING_STATUS_OPT_IN"
         OPT_OUT = 32, "RQC_OPTING_STATUS_OPT_OUT"
-        #OPT_IN_PRELIM = 35, "RQC_OPTING_STATUS_OPT_IN_PRELIM"
-        #OPT_OUT_PRELIM = 36, "RQC_OPTING_STATUS_OPT_OUT_PRELIM"
 
     opting_status = models.IntegerField(choices=OptingChoices.choices, default=OptingChoices.UNDEFINED)
     opting_date = models.DateTimeField(auto_now_add=True)
-    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    reviewer = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    @property
     def is_valid(self):
         """
         Return True if the opting decision is valid for the current UTC year.
