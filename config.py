3140248c5bca2a70c26b528eb6daeed60a8d4594
Julius Harms <julius-harms@web.de>
2025-04-11 02:50:18 +0200

    fixed typo in api version

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # API Configuration
 API_BASE_URL = "https://reviewqualitycollector.org/api"
-API_VERSION = "022-10-19"
+API_VERSION = "2022-10-19"
 
 # Request Configuration
 # Timeout value in seconds
