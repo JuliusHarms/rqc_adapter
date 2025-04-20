91ab0e21914ea19747b419b0c778a6dc318566b7
Julius Harms <julius-harms@web.de>
2025-04-20 20:01:31 +0200

    Urls for several functions

--- urls.py
+++ urls.py
@@ -4,8 +4,10 @@ from django.urls import re_path
 
 urlpatterns = [
     re_path(r'^manager/$', views.manager, name='rqc_adapter_manager'),
-    re_path(r'^manager/handle_journal_settings_update$', views.handle_journal_id_settings_update, name='rqc_adapter_handle_journal_settings_update'),
+    re_path(r'^manager/handle_journal_settings_update$', views.handle_journal_settings_update, name='rqc_adapter_handle_journal_settings_update'),
     re_path(r'^$', views.rqc_grading_articles, name='rqc_adapter_rqc_grading_articles'),
     re_path(r'^article/(?P<article_id>\d+)/$', views.rqc_grade_article_reviews, name='rqc_adapter_rqc_grade_article_reviews'),
     re_path(r'^articles/(?P<article_id>\d+)/submit$', views.submit_article_for_grading, name='rqc_adapter_submit_article_for_grading'),
+    re_path(r'^participate_in_rqc/$', views.reviewer_opting_form, name="rqc_adapter_reviewer_opting_form"),
+    re_path(r'^set_reviewer_opting_status/$', views.set_reviewer_opting_status, name='rqc_adapter_set_reviewer_opting_status'),
 ]
