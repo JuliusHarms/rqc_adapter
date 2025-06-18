69f2d8dcf5d4528d118ddf640ff460dcb6e1e3f5
Julius Harms <julius-harms@web.de>
2025-06-18 14:50:35 +0200

    feat: Added new hook content

--- hooks.py
+++ hooks.py
@@ -1,13 +1,18 @@
 from django.template.loader import render_to_string
+from plugins.rqc_adapter import forms
+from plugins.rqc_adapter.models import RQCReviewerOptingDecision
 
+
+def render_reviewer_opting_link(context):
+    request = context['request']
+    return  render_to_string('rqc_adapter/reviewer_dashboard_opting_link.html', context={})
+
+def render_rqc_grading_action(context):
+    request = context['request']
+    string = render_to_string('rqc_adapter/grading_action.html', context={'article': context['article']})
+    return string
 
 def render_reviewer_opting_form(context):
     request = context['request']
-    article = context['article']
-    return  render_to_string('rqc_adapter/reviewer_opting_form.html', context={'article': article})
-
-def render_rqc_grading_task(context):
-    request = context['request']
-    string = render_to_string('rqc_adapter/grading_task.html', context={})
-    print(string)
-    return string
+    form = forms.ReviewerOptingForm(initial={'status_selection_field': RQCReviewerOptingDecision.OptingChoices.OPT_IN})
+    return render_to_string('rqc_adapter/reviewer_opting_form.html', context={'request': request, 'form': form})
