8cad20526a788c777302f9367811559f6d9e0e18
Julius Harms <julius-harms@web.de>
2025-08-01 01:16:03 +0200

    refactored manager.html page and journal settings form. Api key is now being hidden and visual formatting is better.

--- forms.py
+++ forms.py
@@ -1,21 +1,29 @@
+"""
+© Julius Harms, Freie Universität Berlin 2025
+"""
+
 from django import forms
 from django.core.validators import RegexValidator
 
 from plugins.rqc_adapter.models import RQCReviewerOptingDecision
 from plugins.rqc_adapter.rqc_calls import call_mhs_apikeycheck
 
-
-class DummyManagerForm(forms.Form):
-    dummy_field = forms.CharField()
-
 class RqcSettingsForm(forms.Form):
-    journal_id_field = forms.IntegerField(error_messages={'invalid': 'Journal ID must be a number'})
+    journal_id_field = forms.IntegerField(error_messages={'invalid': 'Journal ID must be a number'}, label='RQC journal ID', \
+                                          help_text='An integer that is supplied when you register a journal at RQC.'
+                                                    ' You may need to ask your publisher to create one at'
+                                                    ' <a href="https://reviewqualitycollector.org" target="_blank" rel="noopener noreferrer">reviewqualitycollector.org</a> '
+                                                    'or use RQC\'s Demo mode to try it out yourself.')
     journal_api_key_field = forms.CharField(
         max_length=64, min_length=1,
+        widget=forms.PasswordInput,
         validators=[
             RegexValidator(
                 regex='^[0-9A-Za-z]+$',
-                message='The API key must only contain alphanumeric characters.')])
+                message='The API key must only contain alphanumeric characters.')], label='RQC secret journal API key', \
+                help_text='An alphanumeric string created by RQC upon request.' 
+                          ' This value is secret and is used for authentication.'            
+                          ' Ask your publisher to tell you yours or use RQC\'s Demo mode and create one yourself.')
 
     #Validate submitted journal_id and api_key together
     #should there be an option to only submit one or the other?
