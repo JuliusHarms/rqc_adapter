5f23977bb63f09645bb879a1573323faba97e115
Julius Harms <julius-harms@web.de>
2025-09-13 19:19:36 +0200

    added correct logic to events.py to now add the current opting status to assignments if it exists

--- events.py
+++ events.py
@@ -42,7 +42,11 @@ def implicit_call_mhs_submission(**kwargs) -> dict | None:
     api_key = credentials.api_key
     submission_id = article.pk #TODO change sub id to something else?
     post_data = fetch_post_data(user=request.user, article=article, journal= request.journal)
-    return call_mhs_submission(journal_id=journal_id, api_key=api_key, submission_id=submission_id, post_data=post_data, article=article)
+    return call_mhs_submission(journal_id=journal_id,
+                               api_key=api_key,
+                               submission_id=submission_id,
+                               post_data=post_data,
+                               article=article)
 
 # Executed when ON_REVIEWER_ACCEPTED event happens (when a reviewer accepts a review assignment).
 def create_review_assignment_opting_decision(**kwargs):
@@ -51,12 +55,27 @@ def create_review_assignment_opting_decision(**kwargs):
     :param kwargs: Contains ReviewAssignment object
     """
     review_assignment = kwargs.get("review_assignment")
+    journal = review_assignment.journal
+    # Don't create Review Assignment Opting Decisions if no API credentials are present
+    try:
+        RQCJournalAPICredentials.objects.get(journal=journal)
+    except RQCJournalAPICredentials.DoesNotExist:
+        return None
+
     if not review_assignment:
         logger.error('Could not create RQC opting decision: review_assignment is required')
         return None
     try:
-        # Create with default status of undefined
-        RQCReviewerOptingDecision.objects.get_or_create(review_assignment=review_assignment)
+        decision = RQCReviewerOptingDecision.objects.filter(reviewer=review_assignment.reviewer,
+                                                             journal=journal,
+                                                             opting_date__year=utc_now().year).first()
+        if decision is not None:
+            opting_status = decision.opting_status
+            RQCReviewerOptingDecision.objects.get_or_create(review_assignment=review_assignment,
+                                                            opting_status=opting_status)
+        else:
+            # Create with default status of undefined
+            RQCReviewerOptingDecision.objects.get_or_create(review_assignment=review_assignment)
     except Exception as e:
         logger.error(f'Could not create RQC opting decision for review assignment: {e}')
         return None
