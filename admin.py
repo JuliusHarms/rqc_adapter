536c98a973042d8f8db133ec0044e613f7ecd0c4
Julius Harms <julius-harms@web.de>
2025-09-13 19:17:01 +0200

    opting_date is now shown in the admin panel as read-only

--- admin.py
+++ admin.py
@@ -9,18 +9,15 @@ from plugins.rqc_adapter.models import RQCReviewerOptingDecision, RQCDelayedCall
 
 
 class RQCReviewerOptingDecisionAdmin(admin.ModelAdmin):
-    list_display = ('reviewer', 'journal', 'opting_status', 'opting_date')
+    list_display = ('reviewer', 'journal', 'opting_status')
+    readonly_fields = ('opting_date',)
 
 
 class RQCReviewerOptingDecisionForReviewAssignmentAdmin(admin.ModelAdmin):
-    list_display = ('reviewer', 'review_assignment', 'opting_status')
+    list_display = ('review_assignment', 'opting_status')
 
 class RQCDelayedCallAdmin(admin.ModelAdmin):
     list_display = ('article', 'remaining_tries', 'last_attempt_at', 'failure_reason')
-
-#TODO should i even do this?
-class RQCJournalAPICredentialsAdmin(admin.ModelAdmin):
-    list_display = ('journal', 'journal_id', 'api_key')
 
 admin.site.register(RQCReviewerOptingDecision, RQCReviewerOptingDecisionAdmin)
 admin.site.register(RQCReviewerOptingDecisionForReviewAssignment, RQCReviewerOptingDecisionForReviewAssignmentAdmin)
