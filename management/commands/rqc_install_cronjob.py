c54eb6c6d0703c78633bc872fef6edde50fcd1d0
Julius Harms <julius-harms@web.de>
2025-09-22 11:05:26 +0200

    reworked and improved install cron command. Added options to see status and to uninstall.

--- management/commands/rqc_install_cronjob.py
+++ management/commands/rqc_install_cronjob.py
@@ -1,8 +1,12 @@
 """
 © Julius Harms, Freie Universität Berlin 2025
+
+This command installs the cronjob that makes delayed calls to the RQC API.
 """
 
 import os
+import shutil
+
 from utils.logger import get_logger
 
 from django.core.management.base import BaseCommand
@@ -20,43 +24,125 @@ class Command(BaseCommand):
     Installs the cron task for retrying failed RQC calls.
     """
 
-    help = "Installs the cron task for retrying failed RQC calls."
+    help = ("Installs the cron task for retrying failed RQC calls. You can "
+            "customize when delayed calls to RQC are made."
+            "Default is '0 8 * *' which means calls are made at 8am every"
+            "day. ")
 
     def add_arguments(self, parser):
-        parser.add_argument('--action', default="")
+        parser.add_argument(
+            '--action',
+            choices=['install', 'remove', 'status'],
+            default='install',
+            help='Action to perform: install, remove, or check status of the cronjob'
+        )
+        parser.add_argument(
+            '--time',
+            type=int,
+            default=8,
+            help='Time at which the daily RQC cronjob is run. Default is 8 for 8am.'
+                 'The value should be between 0 and 23.)'
+        )
 
-    def handle(self, *args, **options):
-        """
-        Installs Cron
-        :param args: None
-        :param options: None
-        :return: None
-        """
-        if not os.path.isfile('/usr/bin/crontab'):
-            self.stdout.write(
-                self.style.WARNING('WARNING: /usr/bin/crontab not found. Could not install RQC cronjob.')
+    def get_crontab(self):
+        if not shutil.which('crontab'):
+            self.stdout.write(self.style.ERROR((
+                'WARNING: crontab command not found. Please install cron on your system.'
+                    )
+                )
             )
-            return
-
+            return None
         if not crontab:
             self.stdout.write(
-                self.style.WARNING('WARNING: crontab python module is not installed. Could not install RQC cronjob.')
+                self.style.WARNING('WARNING: crontab python module is not installed.')
             )
+            return None
+        tab = crontab.CronTab(user=True)  # load current user as cron user
+        return tab
+
+    @staticmethod
+    def find_rqc_cronjob(tab):
+        command_name = 'rqc_make_delayed_calls'
+        for job in tab:
+            if command_name in job.command:
+                return job
+        return None
+
+    def install_rqc_cronjob(self, time=8):
+        """Installs RQC cronjob.
+        :param time: time of cronjob to install. Value between 0 and 23
+        """
+        tab = self.get_crontab()
+
+        if tab is None:
+            return
+        if  time < 0 or time > 23:
+            self.stdout.write(self.style.ERROR('Could not install RQC cronjob. '
+                                               'Please enter a time value between 0 and 23'))
             return
 
-        tab = crontab.CronTab(user=True) #load current user as cron user
+        # Get command
         virtualenv = os.environ.get('VIRTUAL_ENV', None)
         django_command = "{0}/manage.py {1}".format(settings.BASE_DIR, 'rqc_make_delayed_calls')
         if virtualenv:
-            command = '%s/bin/python3 %s' % (virtualenv, django_command)
+            command = '/%s/bin/python3 %s' % (virtualenv, django_command)
         else:
             command = '%s' % django_command
         cron_job = tab.new(command)
-        #TODO shift cron time? or let users set the cron job time?
-        cron_job.setall('0 8 * * *')
+
+        # Set time
+        if time is not None:
+            cron_job.setall(f'0 {time} * * *')
+        else:
+            cron_job.setall('0 8 * * *')
         tab.write()
+
+        # Write status
         self.stdout.write(
             self.style.SUCCESS('Successfully installed RQC cronjob.')
         )
         logger.info(f'Successfully installed RQC cronjob.')
         self.stdout.flush()
+
+    def remove_rqc_cronjob(self):
+        """Removes RQC cronjob."""
+        tab = self.get_crontab()
+        rqc_cronjob = self.find_rqc_cronjob(tab)
+        if tab is None:
+            self.stdout.write(self.style.ERROR('Could not remove RQC cronjob. Perhaps RQC cronjob '
+                             'or cron is not installed.')
+                              )
+            return
+        if rqc_cronjob is not None:
+            tab.remove(rqc_cronjob)
+            tab.write()
+            self.stdout.write(self.style.SUCCESS('Successfully removed RQC cronjob')
+                              )
+
+    def show_status(self):
+        """Show the status of the RQC cronjob."""
+        tab = self.get_crontab()
+        if tab is None:
+            self.stdout.write(self.style.ERROR('Could not find crontab. Perhaps cron is not installed.'))
+            return
+        rqc_cronjob = self.find_rqc_cronjob(tab)
+        if rqc_cronjob is None:
+            self.stdout.write(self.style.ERROR('RQC cronjob is not installed.'))
+            return
+        else:
+            self.stdout.write(self.style.SUCCESS('RQC cronjob is installed. '
+                                                 'Current cronjob entry is {}. '
+                               ''.format(rqc_cronjob)))
+            return
+
+    def handle(self, *args, **options):
+        """
+        Manages RQC cronjob based on the specified action and schedule.
+        """
+        action = options['action']
+        if action == 'install':
+            self.install_rqc_cronjob(options['time'])
+        elif action == 'remove':
+            self.remove_rqc_cronjob()
+        elif action == 'status':
+            self.show_status()
